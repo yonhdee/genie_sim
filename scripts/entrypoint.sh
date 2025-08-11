692a306690bd364a3c13e7631737749f06387763
yonhdee <yonh_dee@hotmail.com>
2025-08-11 23:03:47 +0800

    default cyclonedds

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,6 +17,7 @@ echo "source /opt/ros/$ROS_DISTRO/setup.bash" >>~/.bashrc
 echo "export SIM_ASSETS=/root/assets" >>~/.bashrc
 echo "export SIM_REPO_ROOT=/root/workspace/main" >>~/.bashrc
 
+echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >>~/.bashrc
 echo "export ROS_DISTRO=humble" >>~/.bashrc
 echo "export ROS_LOCALHOST_ONLY=1" >>~/.bashrc
 
